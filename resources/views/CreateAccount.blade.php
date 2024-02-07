267942b93da75a782f8e99478e2a39c36c33a378
Ayman benchalh <aymanbenchalh53@gmail.com>
2024-02-08 00:58:01 +0100

    edite fils css and I edite page Create Account

--- resources/views/CreateAccount.blade.php
+++ resources/views/CreateAccount.blade.php
@@ -3,7 +3,16 @@
 @section("content")
 
 
-<div>
-  heloo create acoount    <!-- Waste no more time arguing what a good man should be, be one. - Marcus Aurelius -->
+<div class="containerCreateAcount">
+  <div class="party2">
+<div class="contentImagCreateAcount"><img src="{{ url('Image/foottleimg.svg') }}" alt="image"></div>
+<div class="contantForme">
+  <div class="titleCreatAcourt">Sing Up</div>
+  <form action="" method="POST">
+    
+  </form>
+</div>
+  </div>
+  <!-- Waste no more time arguing what a good man should be, be one. - Marcus Aurelius -->
 </div>
 @endsection
--- resources/views/loginpage.blade.php
+++ resources/views/loginpage.blade.php
@@ -3,7 +3,7 @@
 @section("content")
 
 
-<div>
-  heloo login    <!-- Waste no more time arguing what a good man should be, be one. - Marcus Aurelius -->
+<div class="containerCreateAcount">
+      <!-- Waste no more time arguing what a good man should be, be one. - Marcus Aurelius -->
 </div>
 @endsection
